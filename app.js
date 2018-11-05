b590edd0686010791024a140e42e862904696c20
Abel George <abel.paul.george@gmail.com>
2018-11-05 13:24:54 -0500

    added json parse

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+app.set("view engine", "ejs");
 
 
 app.get("/results",function(req,res){
     request("http://www.omdbapi.com/?s=spiderman&apikey=thewdb",function(error,response,body){
         if(!error && response.statusCode == 200){
-            var results = JSON.parse(body)
-            res.send(results["Search"][0]["Title"]);
+            let data = JSON.parse(body)
+            res.render("results", { data: data});
         }
     })
 })
